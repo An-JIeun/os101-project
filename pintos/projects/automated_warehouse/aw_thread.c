504b57702bd3a569c2e49c4553634d91849d5cbe
Jieun Ahn <68333747+An-JIeun@users.noreply.github.com>
2024-05-07 21:50:52 +0900

    Update aw_thread.c

--- pintos/projects/automated_warehouse/aw_thread.c
+++ pintos/projects/automated_warehouse/aw_thread.c
@@ -26,9 +26,6 @@
 extern struct list* blocked_threads;
 
 void block_thread(){
-    // You must implement this
-    
-    // Code below is example
     enum intr_level old_level;
     old_level = intr_disable ();
     struct thread *cur = thread_current();
@@ -37,20 +34,12 @@ void block_thread(){
     intr_set_level (old_level);
 }
 
-/**
- * A function unblocking all blocked threads in "blocked_threads" 
- * It must be called by central control thread
- */
 
 void unblock_threads(){
-    // Disable interrupts to ensure atomicity
+
     enum intr_level old_level;
     old_level = intr_disable ();
-    
-    // Acquire lock to access the blocked threads list
     lock_acquire(&filesys_lock);
-    
-    // Iterate through the blocked threads list and unblock each thread
     while (!list_empty(&blocked_threads)) {
         struct list_elem *e = list_pop_front(&blocked_threads);
         struct thread *t = list_entry(e, struct thread, elem);
